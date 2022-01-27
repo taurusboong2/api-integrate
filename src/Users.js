b549d68dfbc484dadd22b6af213b11cae87fb39c
taurusboong2 <taurusboong2@gmail.com>
2022-01-27 15:59:50 +0900

    useAsync without react-async

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,7 @@
-import React, { useEffect, useReducer } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import useAsync from './useAsync';
+import User from './User';
 
 async function getUsers() {
   const response = await axios.get('https://jsonplaceholder.typicode.com/users');
@@ -8,23 +9,27 @@ async function getUsers() {
 }
 
 function Users() {
-  const [state, refetch] = useAsync(getUsers, []);
+  const [userId, setUserId] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
 
-  const { loading, data: users, error } = state; // state.data 를 users 키워드로 조회
+  const { loading, data: users, error } = state;
+  // state.data 를 users 키워드로 조회
 
   if (loading) return <div>로딩중..</div>;
   if (error) return <div>에러가 발생했습니다</div>;
-  if (!users) return null;
+  if (!users) return <button onClick={refetch}>불러오기</button>;
+
   return (
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)} style={{ cursor: 'pointer' }}>
             {user.username} ({user.name})
           </li>
         ))}
       </ul>
       <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
